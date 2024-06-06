ca91bd392036d170ef5d6cb8252cf64e3aa07884
marufremoncse <marufremon.cse14.2@gmail.com>
2024-06-06 17:10:20 +0600

    refix

--- chat-sdk-app-telcobright/src/main/java/notification/NotificationCancelActivity.java
+++ chat-sdk-app-telcobright/src/main/java/notification/NotificationCancelActivity.java
@@ -55,10 +55,10 @@ public class NotificationCancelActivity extends AppCompatActivity {
         ChatSDK.mediaStop();
 //        Flashphoner.init(this);
         String receiverNumber = getIntent().getStringExtra("senderNumber");
-        roomName = ChatSDK.auth().getCurrentUserEntityID();
+        roomName = ChatSDK.auth().getCurrentUserEntityID().split("@")[0];
         HashMap<String, Object> newMessage = new HashMap<>();
         String threadEntityID = receiverNumber + "@localhost";
-        String senderId = ChatSDK.auth().getCurrentUserEntityID()+ "@localhost";
+        String senderId = ChatSDK.auth().getCurrentUserEntityID();
         HashMap<String, HashMap<String, String>> userIds = new HashMap<String, HashMap<String, String>>();
         HashMap<String, String> users = new HashMap<String, String>();
         users.put(threadEntityID, receiverNumber);
